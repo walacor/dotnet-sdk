6805d045de1b88ccb44f568aadff446f98efec3a
keshish <garo.keshish@gmail.com>
2025-09-24 21:24:18 +0200

    Update SDK config

--- Walacor_SDK.Demo/Program.cs
+++ Walacor_SDK.Demo/Program.cs
@@ -12,3 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Running Walacor SDK Demo...");
+    }
+}

898ba589a34fc3a097c26c0b2f7a2581ec2b4313
keshish <garo.keshish@gmail.com>
2025-11-13 12:11:52 +0400

    test

--- Walacor_SDK.Demo/Program.cs
+++ Walacor_SDK.Demo/Program.cs
@@ -21,7 +21,7 @@ class Program
     public static async Task Main(string[] args)
     {
         Console.WriteLine("Hello Walacor!");
-        var svc = new WalacorService("x", "y", "z123");
+        var svc = new WalacorService("x", "y", "z");
 
         //var dataTypes = await svc.SchemaService.GetDataTypesAsync();
         //var pagf = await svc.SchemaService.GetPlatformAutoGenerationFieldsAsync();
